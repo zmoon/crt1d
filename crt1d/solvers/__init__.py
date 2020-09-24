0f49bad5df87ce494a08f62468bcc73740e3f505
zmoon <zmoon92@gmail.com>
2020-09-23 23:06:27 -0400

    Solver variables table sort of working

--- crt1d/solvers/__init__.py
+++ crt1d/solvers/__init__.py
@@ -2,12 +2,109 @@
 These solvers are written as functions that can be called outside the model,
 by supplying the necessary keyword arguments.
 
-%(params)s
-""" 
-# hack module docstring
-__doc__ %= {"params": "hi there"}
+Solver variables
+----------------
+
+%(param_table)s
+
+"""
 
 __all__ = ['available_schemes', 'res_keys_all_schemes']
+
+
+# from collections import namedtuple
+# _Vmd = namedtuple("vmd", "")  # TODO: or use dataclass for defaults?
+
+def _load_variable_metadata():
+    """Load the variable info from the yml file."""
+    from pathlib import Path
+    import yaml
+
+    p = Path(__file__).parent/"variables.yml"
+    with open(p, "r") as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+
+    in_ = []
+    for vn, vn_data in data["in"].items():
+        vn_data["name"] = vn
+        in_.append(vn_data)
+
+    out = []
+    for vn, vn_data in data["out"].items():
+        vn_data["name"] = vn
+        out.append(vn_data)
+
+    return {"in": in_, "out": out}
+
+
+def _param_entry(d):
+    """Form a docstring parameters entry and return as string."""
+    # required
+    name = d["name"]
+    type_ = d["type"]
+    desc = d["desc"]
+    # optional
+    shape = d.get("shape")
+    units = d.get("units")
+    param = d.get("param", False)
+
+    ind = " "*4  # indent
+    s_shape = f"{ind}*shape*: {shape}\n\n" if shape is not None else ""
+    s_units = f"{ind}*units*: {units}\n\n" if units is not None else ""
+    s_param = ""  # TODO: maybe change?
+    s_desc = "\n\n".join(f"{ind}{line}" for line in desc.splitlines())
+
+    return f"""
+{name} : {type_}
+
+{s_shape}{s_units}{s_param}{s_desc}
+    """
+
+
+def _list_table_entry(d):
+    """Form a reST list table entry and return as string."""
+    # required
+    name = d["name"]
+    type_ = d["type"]
+    desc = d["desc"]
+    # optional
+    shape = d.get("shape")
+    units = d.get("units")
+    param = d.get("param", False)
+
+    # ind = " "*3  # indent
+
+    s_desc = desc.splitlines()[0]  # temporary hack
+    return f"""
+   * - ``{name}``
+     - {units}
+     - ``{shape}``
+     - {s_desc}
+     """.rstrip()
+
+def _all_params_list_table(vmd_list):
+    header = """
+.. list-table::
+   :widths: 25 25 20 70
+   :header-rows: 1
+
+   * - name
+     - units
+     - shape
+     - desc
+   """.lstrip()
+    return header + "\n".join(_list_table_entry(d) for d in vmd_list)
+
+
+_vmd = _load_variable_metadata()
+
+# hack module docstring
+# include all params
+__doc__ %= {
+    # "param_in": "\n".join(_param_entry(v) for v in _vmd["in"]), 
+    # "param_out": "\n".join(_param_entry(v) for v in _vmd["out"]),
+    "param_table": _all_params_list_table(_vmd["in"]),
+}
 
 
 def _get_solver_module_names():
