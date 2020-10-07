874c23d4ad2cdbd069c71194154fa229af8ba460
zmoon92 <zmoon92@gmail.com>
2020-10-07 19:51:11 -0400

    fix missing import

--- crt1d/utils.py
+++ crt1d/utils.py
@@ -1,7 +1,6 @@
 """
 Some utility functions, mostly for internal use.
 """
-
 # credit: https://github.com/lukelbd/proplot/blob/master/proplot/internals/docstring.py
 # but for now not using a global snippets dict
 def add_snippets(func, snippets):
@@ -9,6 +8,8 @@ def add_snippets(func, snippets):
     un-indented snippets from the global `snippets` dictionary. This function
     uses ``%(name)s`` substitution rather than `str.format` substitution so
     that the `snippets` keys can be invalid variable names."""
+    import inspect
+
     func.__doc__ = inspect.getdoc(func)
     if func.__doc__:
         func.__doc__ %= {key: value.strip() for key, value in snippets.items()}
@@ -33,4 +34,3 @@ def cf_units_to_tex(s: str):
     s_new = re.sub(r"-?\d", expify, s)
 
     return s_new
-
