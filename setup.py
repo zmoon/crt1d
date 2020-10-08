02e2578b5c49edabc57e365696311b848aa65da1
zmoon92 <zmoon92@gmail.com>
2020-10-07 21:47:33 -0400

    fix include_package_data (hopefully)

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 # setup.py needed in order to have a pip editable install
 from setuptools import setup
 
-setup(use_scm_version=True)  # config is in setup.cfg
+setup(use_scm_version=True, include_package_data=True)  # config is in setup.cfg
+# with `include_package_data`, setuptools_scm will include all Git-tracked files
