94276f9163d5f2a749f409e4a648a2ec25290d5d
zmoon92 <zmoon92@gmail.com>
2020-10-22 10:58:05 -0400

    Add some inputs for sp2

--- crt1d/data/external.py
+++ crt1d/data/external.py
@@ -10,8 +10,42 @@ def leaf_ps5():
     raise NotImplementedError
 
 
-def solar_sp2():
-    """Run SPCTRAL2 from Python package ``solar_utils`` (``SolarUtils`` on PyPI)."""
+def solar_sp2(dt, lat, lon, *, temp=25, pres=1000, utcoffset=0):
+    """Run SPCTRAL2 from Python package ``solar_utils`` (``SolarUtils`` on PyPI).
+
+    Parameters
+    ----------
+    dt : datetime.datetime
+        In UTC, but with timezone not specified. (UTC offset specified separately!)
+    lat, lon : float
+        Latitude and longitude of the location (deg.).
+
+    utcoffset : int
+        UTC offset, e.g., -5 for US Eastern Standard Time (EST)
+
+    temp : float
+        Air temperature at the location (deg. C).
+    pres : float
+        Amospheric surface pressure at the location (hPa/mb).
+
+    """
     import solar_utils
+
+    # construct inputs for solar_utils.spectrl2
+    units = 1
+    location = [lat, lon, utcoffset]
+    list_datetime = [dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second]
+    weather = [pres, temp]
+
+    # run SOLPOS to get angles
+    (zenith, azimuth), _ = solar_utils.solposAM(location, list_datetime, weather)
+
+    # run SPCTRAL2 (following their readme example)
+    specdif, specdir, specetr, specglo, specx = solar_utils.spectrl2(
+        units, location, list_datetime, weather, orientation, atmospheric_conditions, albedo
+    )
+
+    # construct dataset
+    # with time variable and solar angles!
 
     raise NotImplementedError
