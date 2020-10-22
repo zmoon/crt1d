863965e529e4e651c19ba0c378dcc1c163d0d979
zmoon92 <zmoon92@gmail.com>
2020-10-22 10:40:14 -0400

    Add stub fns for running SPCTRAL2 and PROSPECT

---
+++ crt1d/data/external.py
@@ -0,0 +1,17 @@
+"""
+Create spectral input data using external packages.
+"""
+
+
+def leaf_ps5():
+    """Run PROSPECT from Python package ``prosail`` to generate leaf spectra."""
+    import prosail
+
+    raise NotImplementedError
+
+
+def solar_sp2():
+    """Run SPCTRAL2 from Python package ``solar_utils`` (``SolarUtils`` on PyPI)."""
+    import solar_utils
+
+    raise NotImplementedError
